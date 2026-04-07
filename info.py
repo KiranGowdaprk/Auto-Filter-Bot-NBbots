05fc7ef20b2dd39f125c6d03135d52b7638b5980
KiranGowdaprk <107700933+KiranGowdaprk@users.noreply.github.com>
2026-04-07 17:46:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ API_ID = int(environ.get('API_ID', '35057086'))
 API_HASH = environ.get('API_HASH', '344c241bb482993a8b318848421319f3')
 BOT_TOKEN = environ.get('BOT_TOKEN', "8797721372:AAF7DzRgrT2larpcZpoaTjx7A1RxDPm-n0Y")
 
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+CACHE_TIME = int(environ.get('CACHE_TIME', 720))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 PICS = (environ.get('PICS', 'https://i.ibb.co/ch3n9wsf/x.jpg')).split() 
